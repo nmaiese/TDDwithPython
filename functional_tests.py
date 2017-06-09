1aaf6e4abbc8456fde6cdbf41bf375272199e0f0
Aniello Maiese <aniello.maiese@mecglobal.com>
2017-06-09 22:59:55 +0200

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 import unittest
 
@@ -7,7 +8,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(4)
 
     def tearDown(self):
         self.browser.quit()
@@ -28,19 +29,18 @@ class NewVisitorTest(unittest.TestCase):
 
         self.assertIn('To-Do', header_text)
 
+
         # She is invited to enter a to-do item straight away
-        input_box =self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        input_box.send_keys('Buy peacock feathers')
+        # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
-        input_box.send_keys(Keys.ENTER)
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # When she hits enter, she is taken to a new URL, and now the page lists "1: Buy peacock feathers"
+        # as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -48,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-
+        time.sleep(1)
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
